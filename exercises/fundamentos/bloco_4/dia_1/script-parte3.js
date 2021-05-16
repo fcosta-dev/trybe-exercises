d3626d9ba760e5339d46624149be31837a146ada
fgosuen <fgosuendacosta@gmail.com>
2021-05-16 08:32:51 -0300

    Adição dos arquivos de conteúdos

---
+++ exercises/fundamentos/bloco_4/dia_1/script-parte3.js
@@ -0,0 +1,24 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+/**Utilize o operador typeof para imprimir qual o tipo das variáveis patientId , isEnrolled , patientInfo e patientEmail*/
+console.log(typeof(patientId))
+patientId = '50'
+console.log(typeof patientId)
+console.log(typeof isEnrolled )
+console.log(typeof patientInfo )
+console.log(typeof patientEmail )
+console.log(typeof patientAge) 
+
+
+let base = "5"
+let altura = "8"
+let area = base * altura
+console.log("A area é " + area)
+
+perimetro = base + altura
---
+++ exercises/fundamentos/bloco_4/dia_1/script.js
@@ -0,0 +1,19 @@
+/*Crie uma constante chamada name e atribua a ela o seu nome (Exemplo: Carolina); */
+const name = "Fernando";
+/*Crie uma constante chamada birthCity e atribua a ela a sua cidade natal;*/
+const birthCity = "Uberlândia";
+/*Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu;*/
+let birthYear = "40";
+
+/*Utilize o console.log() para imprimir as constantes e variáveis que você criou; */
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
+
+/*Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!*/
+birthYear = "2030"
+console.log(birthYear);
+
+/*Altere o valor atribuído à constante birthCity . Faça um console.log(birthCity) novamente! */
+birthCity = "São Paulo" 
+
