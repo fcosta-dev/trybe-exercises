539bf6aa926d01f9278c51b173aba80eec15abe3
fgosuen <fgosuendacosta@gmail.com>
2021-05-16 17:51:34 -0300

    Adicionando exercício 6

--- exercises/fundamentos/bloco_4/dia_1/exercicios/pratica.js
+++ exercises/fundamentos/bloco_4/dia_1/exercicios/pratica.js
@@ -75,4 +75,32 @@ else {
 }
 console.log("Os angulos representam um triangulo? R: " + ehTriangulo);
 
-
+//Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+//Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+//Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+//Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+//Exemplo: bishop (bispo) -> diagonals (diagonais)
+let nomePecaXadrex = "bispo";
+nomePecaXadrex = nomePecaXadrex.toLowerCase();
+switch (nomePecaXadrex){
+    case 'rei':
+        console.log("Escolhido o Rei: O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.")
+        break;
+    case 'rainha':
+        console.log("Escolhido a Rainha: A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.")
+        break;
+    case 'bispo':
+        console.log("Escolhido o Bispo: O bispo move-se ao longo da diagonal. Não pode pular outras peças.")
+        break;
+    case 'cavalo':
+        console.log("Escolhido o Cavalo: É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.")
+        break;
+    case 'torre':
+        console.log("Escolhido a Torre: A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.")
+        break;
+    case 'peão':
+        console.log("Escolhido o Peão: O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.")
+        break;
+    default:
+        console.log("Peça incorreta. Seguem opções: rei, rainha, bispo, cavalo, torre, peão.")
+}
