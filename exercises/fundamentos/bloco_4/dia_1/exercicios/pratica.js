09a195a93b14276951d5f6ebd58b4a35fdcc8b57
fgosuen <fgosuendacosta@gmail.com>
2021-05-16 17:28:39 -0300

    Subindo exercícios 1 a 5

--- exercises/fundamentos/bloco_4/dia_1/exercicios/pratica.js
+++ exercises/fundamentos/bloco_4/dia_1/exercicios/pratica.js
@@ -60,12 +60,19 @@ console.log("O valor " + valor + " é " + resultadoValor + ".")
 //Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 //Para os ângulos serem de um triângulo válido, a soma dos três devem ser 180 graus.
 //Um ângulo será considerado inválido se não tiver um valor positivo.
-let valorAngulo1 = 3; 
-let valorAngulo2 = 3;
-let valorAngulo3 = 3;
-if (valorAngulo1 < 0 || valorAngulo2 < 0 || valorAngulo3 < 0) {
+let valorAngulo1 = 60; 
+let valorAngulo2 = 60;
+let valorAngulo3 = 60;
+let ehTriangulo = false;
+if (valorAngulo1 > 0 && valorAngulo2 > 0 && valorAngulo3 > 0) {
+    if ((valorAngulo1 + valorAngulo2 + valorAngulo3) == 180) {
+        ehTriangulo = true;
+        console.log("A soma dos angulos é igual a 180 graus!");
+    }
+}
+else {
+    console.log("Um dos angulos é negativo!");
+}
+console.log("Os angulos representam um triangulo? R: " + ehTriangulo);
 
-}
-if ((valorAngulo1 + valorAngulo2 + valorAngulo3) <= 180) {
-    console.log("")
-}
+
