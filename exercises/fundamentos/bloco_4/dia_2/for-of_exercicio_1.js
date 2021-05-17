3f391f0995614a2020d8e949a6f5f3225dfff344
fgosuen <fgosuendacosta@gmail.com>
2021-05-17 15:59:52 -0300

    Adição exercicios 1 - For Of

---
+++ exercises/fundamentos/bloco_4/dia_2/for-of_exercicio_1.js
@@ -0,0 +1,5 @@
+// Utilize o for/of para imprimir os elementos da lista names com o console.log() :
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+for (let nomes of names) {
+    console.log(nomes)
+}
