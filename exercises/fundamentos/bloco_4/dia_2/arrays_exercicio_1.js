0d966a4ff3d8e274d69f9e81f0a612aea452ab0f
fgosuen <fgosuendacosta@gmail.com>
2021-05-17 10:12:21 -0300

    Adição de exercicios de conteúdo 1 a 3

---
+++ exercises/fundamentos/bloco_4/dia_2/arrays_exercicio_1.js
@@ -0,0 +1,5 @@
+// Obtenha o valor "Serviços" do array menu :
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
---
+++ exercises/fundamentos/bloco_4/dia_2/arrays_exercicio_3.js
@@ -0,0 +1,5 @@
+// Exemplo 1 - Adicione o valor "Contato" no final do array menu :
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+console.log(menu);
+menu.push('Contato')
+console.log(menu);
