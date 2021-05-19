7f9a1b7bf2721183f3e4208751e8987f3b91ee98
fgosuen <fgosuendacosta@gmail.com>
2021-05-19 07:52:38 -0300

    Atualização 4.2

--- exercises/fundamentos/bloco_4/dia_2/bonus.js
+++ exercises/fundamentos/bloco_4/dia_2/bonus.js
@@ -12,15 +12,15 @@ for (let index = 1; index < numbers.length; index += 1) {
 console.log(numbers);
 
 // Ordene o array numbers em ordem decrescente e imprima seus valores;
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for (let index = 1; index < numbers.length; index += 1) {
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 1; index < numbers2.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (numbers[index] > numbers[secondIndex]) {
-      let position = numbers[index];
-      numbers[index] = numbers[secondIndex];
-      numbers[secondIndex] = position;
+    if (numbers2[index] > numbers2[secondIndex]) {
+      let position = numbers2[index];
+      numbers2[index] = numbers2[secondIndex];
+      numbers2[secondIndex] = position;
     }
   }
 }
-console.log(numbers);
+console.log(numbers2);
 
