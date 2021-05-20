256c69773bbff42d0e72328442908b1ad6ae22b8
fgosuen <fgosuendacosta@gmail.com>
2021-05-20 12:38:30 -0300

    Adição de exercícios do conteúdo de For/In

---
+++ exercises/fundamentos/bloco_4/dia_4/conteudo_forin.js
@@ -0,0 +1,22 @@
+let cars = ['Saab', 'Volvo', 'BMW'];
+
+for (let index in cars) {
+  console.log(cars[index]);
+}
+
+
+let car = {
+  type: 'Fiat',
+  model: '500',
+  color: 'white',
+};
+
+for (let index in car) {
+  console.log(index, car[index]);
+}
+
+let food = ['hamburguer', 'bife', 'acarajé'];
+for (let value of food) {
+  console.log(value);
+};
+//resultado: hamburguer, bife, acarajé;
