22cccba5febec0bc4fe9a86e03ce2ce7483fb951
fgosuen <fgosuendacosta@gmail.com>
2021-05-20 13:12:16 -0300

    Adição dos exercícios 4 e 5 da Parte I

--- exercises/fundamentos/bloco_4/dia_4/exercicios_parte1_objetosforin.js
+++ exercises/fundamentos/bloco_4/dia_4/exercicios_parte1_objetosforin.js
@@ -10,7 +10,7 @@ console.log("EXERCICIO 01 - Bem vinda, " + info.personagem)
 
 
 // 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console.
-info.recorrente = "Sim"
+info.recorrente = 'Sim';
 console.log("EXERCICIO 02 :")
 console.table(info)
 console.log(info)
@@ -22,3 +22,20 @@ for (let key in info) {
 
 
 // 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto.
+for (let key in info) {
+  console.log("EXERCÍCIO 04 - " + info[key]);
+}
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+let info2 = {};
+info2.personagem = info.personagem + " e Tio Patinhas";
+info2.origem = info.origem + " e Christmas on Bear Mountain, Dell's Four Color Comics #178";
+info2.nota = info.nota + " e O último MacPatinhas";
+if (info.recorrente = "sim") {
+  info2.recorrente = "Ambos recorrentes";
+} else {
+  info2.recorrente = info.recorrente + " e Sim";
+}
+console.log(info2)
+console.table(info2)
+
