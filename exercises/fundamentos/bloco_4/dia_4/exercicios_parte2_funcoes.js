0fc80e2c6c1b8a5a7360470f74a69b5c6224fbec
fgosuen <fgosuendacosta@gmail.com>
2021-05-21 07:41:58 -0300

    Adição dos exercícios 1 a 7 da Parte II

--- exercises/fundamentos/bloco_4/dia_4/exercicios_parte2_funcoes.js
+++ exercises/fundamentos/bloco_4/dia_4/exercicios_parte2_funcoes.js
@@ -38,10 +38,11 @@ function retornaIndice(array) {
 }
 console.log(retornaIndice(teste))
 
-//Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 //Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
 //Valor esperado no retorno da função: 6 .
 let arrayDeTeste = [2, 4, 6, 7, 10, 0, -3];
+
 function retornaIndice(array) {
   let menorNumero = array[0];
   let menorIndice = 0;
@@ -55,16 +56,94 @@ function retornaIndice(array) {
 }
 console.log(retornaIndice(arrayDeTeste))
 
-//Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 //Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']; .
 //Valor esperado no retorno da função: Fernanda .
 let arrayDeNomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
 function nomeMaior(array) {
-  let 
-  for (let keys in array) {
-
+  let qtdLetras = 0
+  let indiceMaiorLetras = 0
+  for (let key in array) {
+    if (qtdLetras < array[key].length) {
+      qtdLetras = array[key].length
+      indiceMaiorLetras = key
+    }
   }
-  return "EXERCICIO 04 - O menor indice é o "
+  return "EXERCICIO 04 - O nome com a maior quantidade de caracteres é " + array[indiceMaiorLetras] + " com " + qtdLetras + " caracteres."
 }
 console.log(nomeMaior(arrayDeNomes))
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+//Array de teste: [2, 3, 2, 5, 8, 2, 3]; .
+//Valor esperado no retorno da função: 2 .
+let arraysNumerosQueRepetem = [2, 3, 2, 5, 8, 2, 3];
+
+function numerosQueRepetem(array) {
+  let qtdQueRepete = 0;
+  let nroQueMaisRepete = array[0];
+  
+  for (let keys of array) {
+    numeroAvaliado = array[keys];
+    numeroAvaliadoRepeticoes = 0;
+    for (let index in array) {
+      if (numeroAvaliado == array[index]) {
+        numeroAvaliadoRepeticoes += 1;
+      }
+    }
+    if (qtdQueRepete < numeroAvaliadoRepeticoes) {
+      qtdQueRepete = numeroAvaliadoRepeticoes;
+      nroQueMaisRepete = numeroAvaliado;
+    }
+  }
+  return "EXERCICIO 05 - O número que mais se repete é o " + nroQueMaisRepete + " e ele se repete " + qtdQueRepete + " vezes." 
+}
+console.log(numerosQueRepetem(arraysNumerosQueRepetem))
+
+// 6 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+//Valor de teste: N = 5 .
+//Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
+let n = 5
+
+function somatodos(numero) {
+  let somarNumeros = 0;
+  let descritivo = ""
+  for (let index = 1; index <= numero; index += 1) {
+    if (index != 1) {
+      descritivo += "+" + index
+      somarNumeros += index
+    } else {
+      descritivo += index
+      somarNumeros += index
+    }
+  }
+  return "EXERCICIO 06 - A soma dos números é " + descritivo + " = " + somarNumeros
+}
+console.log(somatodos(n))
+
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+// Valor de teste: 'trybe' e 'be'
+// Valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be') ;
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan') ;
+// Retorno esperado: false
+let stringRecebida1 = "word"
+let stringRecebida2 = "ending"
+
+function analiseStrings(stringPrincipal, stringParaAnalisar) {
+  let stringPrincipalSplitada = stringPrincipal.split('');
+  let stringParaAnalisarSplitada = stringParaAnalisar.split('');
+  let contadorDeNumeros = 0
+  let ehigual = true;
+  for (let index1 = stringParaAnalisarSplitada.length; index1 > 0; index1 -= 1) {
+    contadorDeNumeros += 1
+    if (stringParaAnalisarSplitada[index1 -1] == stringPrincipalSplitada[stringPrincipalSplitada.length - contadorDeNumeros]) {
+      ehigual = true
+    } else {
+      ehigual = false
+    }
+  }
+  return "EXERCICIO 07 - A string '" + stringParaAnalisar + "' é final da string '" + stringPrincipal +"'? " + ehigual
+}
+console.log(analiseStrings(stringRecebida1, stringRecebida2))
