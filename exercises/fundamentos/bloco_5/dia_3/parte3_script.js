215aff9c02e01d3a07878b7135dee44d17f75bde
fgosuen <fgosuendacosta@gmail.com>
2021-05-27 20:19:19 -0300

    Adição dos exercícios 1 a 5

--- exercises/fundamentos/bloco_5/dia_3/parte3_script.js
+++ exercises/fundamentos/bloco_5/dia_3/parte3_script.js
@@ -8,20 +8,43 @@ const myWebpage = document.getElementById('mySpotrybefy');
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
-input.addEventListener("onChange", alteraTexto);
-
-function alteraTexto() {
-  divUm.innerText = input.innerText
-  //elementReceived.innerText
+function mudaTech(event) {
+  const elementTech = document.querySelector('.tech');
+  elementTech.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
 }
+divUm.addEventListener('click', mudaTech);
+divDois.addEventListener('click', mudaTech);
+divTres.addEventListener('click', mudaTech);
+
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento com a classe 'tech';
+input.addEventListener('input', (event) => {
+  elementTech = document.querySelector('.tech'); 
+  divUm.innerText = input.innerText
+})
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+let meuTop3 = document.querySelector('#mySpotrybefy')
+meuTop3.addEventListener('dblclick', vaiParaPortifolio)
 
+function vaiParaPortifolio() {
+  window.location.href = "https://fgosuen.github.io/"
+}
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere a cor do mesmo;
+meuTop3.addEventListener('mouseover', mudaCor)
+meuTop3.addEventListener('mouseout', voltaCor)
+
+function mudaCor(event) {
+  event.target.style.color = 'red';
+}
+
+function voltaCor(event) {
+  event.target.style.color = 'unset';
+}
 
 //Segue abaixo um exemplo do uso de event.target:
 
