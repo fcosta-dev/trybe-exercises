539d916d85b5a604c1184b168b71ce95c3733429
fgosuen <fgosuendacosta@gmail.com>
2021-05-28 07:07:14 -0300

    Adição de resolução do Exercício 02

--- exercises/fundamentos/bloco_5/dia_3/exercicios_script.js
+++ exercises/fundamentos/bloco_5/dia_3/exercicios_script.js
@@ -52,3 +52,15 @@ function checaData(array, contador) {
 // Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
 // Adicione a este botão a ID "btn-holiday" .
 // Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+criaBotao("Feriados")
+function criaBotao(stringRecebida) {
+  const elementDivButtons = document.querySelector('.buttons-container');
+  const Button = document.createElement('button');
+  Button.innerText = stringRecebida;
+  Button.id = 'btn-holiday';
+  elementDivButtons.appendChild(Button);
+}
+
+//Exercício 3:
+//Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+//É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
