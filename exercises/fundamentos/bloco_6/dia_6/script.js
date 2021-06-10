7bac2e508470e9b2f99a299dd8aa11759791b662
fgosuen <fgosuendacosta@gmail.com>
2021-06-10 20:55:51 -0300

    Resolução dos Requisitos 18

--- exercises/fundamentos/bloco_6/dia_6/script.js
+++ exercises/fundamentos/bloco_6/dia_6/script.js
@@ -2,11 +2,22 @@
 const textoLogin = document.getElementById('txt_login');
 const textoSenha = document.getElementById('txt_senha');
 const elementoBotao = document.getElementById('botao_login');
+const elementoAgreement = document.getElementById('agreement')
+const elementoEnviar = document.getElementById('submit-btn')
 
-
+elementoEnviar.disabled = true;
 
 /* EVENTOS */
 elementoBotao.addEventListener('click', validaLogin)
+elementoAgreement.addEventListener('click', checaSubmit)
+
+function checaSubmit() {
+  if (elementoAgreement.checked === true) {
+    elementoEnviar.disabled = false;
+  } else {
+    elementoEnviar.disabled = true;
+  }
+}
 
 /* FUNÇÕES */
 function validaLogin() {
