6232a64a7310f9a6770fe2d11253d89f7ce19d28
fgosuen <fgosuendacosta@gmail.com>
2021-06-10 14:09:58 -0300

    Resolução dos Requisitos 4 a 11

--- exercises/fundamentos/bloco_6/dia_6/script.js
+++ exercises/fundamentos/bloco_6/dia_6/script.js
@@ -1,24 +1,24 @@
 /* ELEMENTOS */
 const textoLogin = document.getElementById('txt_login');
 const textoSenha = document.getElementById('txt_senha');
-const elementoBotao = document.getElementById('txt_senha');
+const elementoBotao = document.getElementById('botao_login');
 
 
 
 /* EVENTOS */
 elementoBotao.addEventListener('click', validaLogin)
 
-
 /* FUNÇÕES */
-function validaLogin {
+function validaLogin() {
   let checaLogin = false;
-  if (textoLogin.Value === 'tryber@teste.com') {
-    if (textoSenha.Value === '123456') {
+  if (textoLogin.value === 'tryber@teste.com') {
+    if (textoSenha.value === '123456') {
       checaLogin = true;
     }
   }
   if (checaLogin === false) {
     alert('Login ou senha inválidos')
+
   } else {
     alert('Olá, Tryber!')
   }
