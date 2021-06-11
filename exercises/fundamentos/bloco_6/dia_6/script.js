fef53c8540dda69c4c4b00a42d65c187f33c1d1a
fgosuen <fgosuendacosta@gmail.com>
2021-06-11 09:22:06 -0300

    Resolução dos Requisitos Bonus 20

--- exercises/fundamentos/bloco_6/dia_6/script.js
+++ exercises/fundamentos/bloco_6/dia_6/script.js
@@ -5,19 +5,16 @@ const elementoBotao = document.getElementById('botao_login');
 const elementoAgreement = document.getElementById('agreement')
 const elementoEnviar = document.getElementById('submit-btn')
 
+const elementoTextArea = document.getElementById('textarea')
+const elementoContador = document.getElementById('counter')
+
 elementoEnviar.disabled = true;
 
 /* EVENTOS */
 elementoBotao.addEventListener('click', validaLogin)
 elementoAgreement.addEventListener('click', checaSubmit)
+elementoTextArea.addEventListener('keyup', contaString)
 
-function checaSubmit() {
-  if (elementoAgreement.checked === true) {
-    elementoEnviar.disabled = false;
-  } else {
-    elementoEnviar.disabled = true;
-  }
-}
 
 /* FUNÇÕES */
 function validaLogin() {
@@ -33,4 +30,18 @@ function validaLogin() {
   } else {
     alert('Olá, Tryber!')
   }
+}
+
+function checaSubmit() {
+  if (elementoAgreement.checked === true) {
+    elementoEnviar.disabled = false;
+  } else {
+    elementoEnviar.disabled = true;
+  }
+}
+
+function contaString() {
+  console.log('teste')
+  let qtdString = elementoTextArea.value.length;
+  elementoContador.innerText = (500-qtdString);
 }
