50684272a27357b25a14643490f3d9bca2811ebb
fgosuen <fgosuendacosta@gmail.com>
2021-06-11 17:15:26 -0300

    Finalização dos exerícios - Parte 1

--- exercises/fundamentos/bloco_7/dia_1/exercicios_parte1.js
+++ exercises/fundamentos/bloco_7/dia_1/exercicios_parte1.js
@@ -14,3 +14,17 @@ testingScope(true);
 
 // Modifique a estrutura da função para que ela seja uma arrow function .
 // Modifique as concatenações para template literals .
+const testingScope = escopo => { 
+  if (escopo === true) { 
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+
