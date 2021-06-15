18264a9478a68b3b3d303236604a8f531e960809
fgosuen <fgosuendacosta@gmail.com>
2021-06-15 19:17:51 -0300

    Adição resolução exercício 01 - Parte 2

---
+++ exercises/fundamentos/bloco_7/dia_3/parte2/exercicio_1.js
@@ -0,0 +1,19 @@
+// Escreva a função addOne para passar nos testes já implementados.
+const assert = require('assert');
+// escreva a função addOne aqui
+const addOne = (array) => {
+  const novoArray = []
+  for (let index =0; index < array.length; index += 1) {
+    novoArray.push(array[index] + 1);
+  }
+  return novoArray
+}
+
+const myArray = [31, 57, 12, 5];
+const unchanged = [31, 57, 12, 5];
+const expected = [32, 58, 13, 6];
+const output = addOne(myArray);
+
+assert.strictEqual(typeof addOne, 'function');
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
