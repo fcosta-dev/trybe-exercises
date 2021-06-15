45e3f0ded8acd6872ca5e4a16d759b7ba2250dbb
fgosuen <fgosuendacosta@gmail.com>
2021-06-15 13:17:45 -0300

    Iniciando os exercícios

---
+++ exercises/fundamentos/bloco_7/dia_3/exercicios.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+// implemente seus testes aqui
