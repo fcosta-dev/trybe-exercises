f5e3eed23d0e4e17d16988b336aaaafd253db7fd
fgosuen <fgosuendacosta@gmail.com>
2021-06-21 14:29:05 -0300

    Estudos de com e sem foreach e find

---
+++ exercises/fundamentos/bloco_8/dia_2/importanciaHofs3.js
@@ -0,0 +1,11 @@
+const numbers = [11, 24, 39, 47, 50, 62, 75, 81, 96, 100];
+let firstMultipleOf5;
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 5 === 0) {
+    firstMultipleOf5 = numbers[index];
+    break;
+  }
+}
+
+console.log(firstMultipleOf5);
+// 50
