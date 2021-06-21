019a3132a217dae3cb0a8140f3a5413e224654c1
fgosuen <fgosuendacosta@gmail.com>
2021-06-21 14:18:32 -0300

    Iniciando estudos dos Conteudos

---
+++ exercises/fundamentos/bloco_8/dia_2/importanciaHofs.js
@@ -0,0 +1,27 @@
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+
+function verifyGrades() {
+  for (let i = 0; i < students.length; i += 1) {
+    const student = students[i];
+    if (student.grade >= 60) {
+      student.approved = 'Aprovado';
+    } else {
+      student.approved = 'Recuperação';
+    }
+  }
+}
+
+verifyGrades();
+
+console.log(students);
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
---
+++ exercises/fundamentos/bloco_8/dia_2/importanciaHofs2.js
@@ -0,0 +1,26 @@
+const students = [
+  { name: 'Maria', grade: 70, approved: '' },
+  { name: 'José', grade: 56, approved: '' },
+  { name: 'Roberto', grade: 90, approved: '' },
+  { name: 'Ana', grade: 81, approved: '' },
+];
+
+function verifyGrades() {
+  students.forEach((student, index) => {
+    if (student.grade >= 60) {
+      students[index].approved = 'Aprovado';
+    } else {
+      students[index].approved = 'Recuperação';
+    }
+  });
+}
+
+verifyGrades();
+
+console.log(students);
+// [
+//   { name: 'Maria', grade: 70, approved: 'Aprovado' },
+//   { name: 'José', grade: 56, approved: 'Recuperação' },
+//   { name: 'Roberto', grade: 90, approved: 'Aprovado' },
+//   { name: 'Ana', grade: 81, approved: 'Aprovado' }
+// ]
