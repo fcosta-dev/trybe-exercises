7f501e9f095f2f156ad76ceb2117a12c3c4f6ab0
fgosuen <fgosuendacosta@gmail.com>
2021-06-21 18:54:38 -0300

    Adição de exercícios de Find e Foreach

---
+++ exercises/fundamentos/bloco_8/dia_2/find.js
@@ -0,0 +1,12 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+//Maneira 01
+const verifyEven = (number) => number % 2 === 0;
+const isEven = numbers.find(verifyEven);
+console.log(isEven); // 30
+console.log(verifyEven(9)); // False
+console.log(verifyEven(14)); // True
+
+// Maneira 02 - sem a necessidade de criar uma nova função.
+const isEven2 = numbers.find((number) => number % 2 === 0);
+console.log(isEven2); // 30
