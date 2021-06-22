91f6c9f18b6453eaad6d06b9d145057461bfd2af
fgosuen <fgosuendacosta@gmail.com>
2021-06-22 17:54:17 -0300

    Resolução dos exercícios 1, 2 e 3

--- exercises/fundamentos/bloco_7/dia_3/exercicios_2.js
+++ exercises/fundamentos/bloco_7/dia_3/exercicios_2.js
@@ -19,10 +19,10 @@ assert.strictEqual(typeof myRemove, 'function');
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
-assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 
 // Verifique se o array passado por parâmetro não sofreu alterações
-assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 4), [1, 2, 3, 4]);
 
 // Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
