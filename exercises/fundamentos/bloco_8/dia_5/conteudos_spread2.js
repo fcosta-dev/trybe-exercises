5a57d1502084e78f7d41aa98a3169a743699d122
fgosuen <fgosuendacosta@gmail.com>
2021-06-24 19:14:00 -0300

    Adicionando conteudo Parametro Rest

--- exercises/fundamentos/bloco_8/dia_5/conteudos_spread2.js
+++ exercises/fundamentos/bloco_8/dia_5/conteudos_spread2.js
@@ -1,11 +1,12 @@
 // Faça uma lista com as suas frutas favoritas
-const specialFruit = ['', '', ''];
+const specialFruit = ['abacaxi', 'melão', 'melancia'];
 
 // Faça uma lista de complementos que você gostaria de adicionar
-const additionalItens = ['', '', ''];
+const additionalItens = ['leite condensado', 'suco de laranja', 'leite'];
 
 const fruitSalad = (fruit, additional) => {
   // Esreva sua função aqui
+  return [...fruit, ...additional]
 };
 
 console.log(fruitSalad(specialFruit, additionalItens));
