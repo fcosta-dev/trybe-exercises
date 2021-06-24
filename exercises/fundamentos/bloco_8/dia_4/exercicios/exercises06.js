f1de0f7af13e2b8fb59701c3fd7f45442087c938
fgosuen <fgosuendacosta@gmail.com>
2021-06-24 11:54:30 -0300

    Finalização dos exercícios

--- exercises/fundamentos/bloco_8/dia_4/exercicios/exercises06.js
+++ exercises/fundamentos/bloco_8/dia_4/exercicios/exercises06.js
@@ -1,17 +1,52 @@
-// 6. - Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas } . Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos , aqui além de reduce será necessário utilizar também a função map . Dica: Você pode acessar o index do array dentro de map , e você pode ver o objeto esperado na constante expected .
-// const assert = require('assert');
-
 const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-function studentAverage() {
-  // escreva seu código aqui
+function studentAverage() { // FAZ A MÉDIA
+  const nomeEMedia = students
+    .map((elemento, index) => ({
+      name: elemento,
+      average: (grades[index]
+        .reduce((acumulador, valorAtual) => acumulador + valorAtual, 0) / grades[index].length)
+    }))
+  
+  return nomeEMedia
 }
+// [
+//   { name: 'Pedro Henrique', average: 7.8 },
+//   { name: 'Miguel', average: 9.2 },
+//   { name: 'Maria Clara', average: 8.8 }
+// ]
 
-const expected = [
-  { name: 'Pedro Henrique', average: 7.8 },
-  { name: 'Miguel', average: 9.2 },
-  { name: 'Maria Clara', average: 8.8 },
-];
+function studentMaiorNota() { // PEGA A MAIOR NOTA
+  const nomeENotaMaior = students
+  .map((elemento, index) => ({
+    name: elemento,
+    maiorNota: grades[index]
+      .reduce((acumulador, valorAtual) => ((acumulador > valorAtual) ? acumulador : valorAtual) ,0)
+  }))
+  return nomeENotaMaior
+}
+// [
+//   { name: 'Pedro Henrique', maiorNota: 10 },
+//   { name: 'Miguel', maiorNota: 10 },
+//   { name: 'Maria Clara', maiorNota: 10 }
+// ]
 
-// assert.deepStrictEqual(studentAverage(), expected);
+function studentMenorNota() { // PEGA A MENOR NOTA
+  const nomeENotaMenor = students
+  .map((elemento, index) => ({
+    name: elemento,
+    menorNota: grades[index]
+      .reduce((acumulador, valorAtual) => (acumulador < valorAtual) ? acumulador : valorAtual ,10)
+  }))
+  return nomeENotaMenor
+}
+// [
+//   { name: 'Pedro Henrique', menorNota: 5 },
+//   { name: 'Miguel', menorNota: 8 },
+//   { name: 'Maria Clara', menorNota: 7 }
+// ]
+
+console.log(studentAverage())
+console.log(studentMaiorNota())
+console.log(studentMenorNota())
