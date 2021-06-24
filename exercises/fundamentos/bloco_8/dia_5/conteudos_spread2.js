bfbfed7767078029947cf9932a46ac8e83127338
fgosuen <fgosuendacosta@gmail.com>
2021-06-24 18:23:12 -0300

    Iniciando os exercícios do Conteudo Spread

---
+++ exercises/fundamentos/bloco_8/dia_5/conteudos_spread2.js
@@ -0,0 +1,11 @@
+// Faça uma lista com as suas frutas favoritas
+const specialFruit = ['', '', ''];
+
+// Faça uma lista de complementos que você gostaria de adicionar
+const additionalItens = ['', '', ''];
+
+const fruitSalad = (fruit, additional) => {
+  // Esreva sua função aqui
+};
+
+console.log(fruitSalad(specialFruit, additionalItens));
