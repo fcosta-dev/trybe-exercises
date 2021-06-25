15bb49c7874996d8a96d2db7af2aaea94d6f39dd
fgosuen <fgosuendacosta@gmail.com>
2021-06-25 14:17:06 -0300

    Finalizando conteudo de Default destructuring

--- exercises/fundamentos/bloco_8/dia_5/x.js
+++
@@ -1,6 +0,0 @@
-const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
-
-// Produza o mesmo resultado acima, porém utilizando array destructuring
-const [saudacao, realizaSaudacao] = saudacoes;
-
-console.log (realizaSaudacao(saudacao)); // Olá
