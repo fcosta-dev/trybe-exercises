97b726bdb4fb7f2f9713779914ba213dbf36d4d5
fgosuen <fgosuendacosta@gmail.com>
2021-06-25 15:28:11 -0300

    Finalizando exercício 2

--- exercises/fundamentos/bloco_8/dia_5/exercises/exercise2.js
+++ exercises/fundamentos/bloco_8/dia_5/exercises/exercise2.js
@@ -1,6 +1,13 @@
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+// Dica: use parâmetro rest .
 const assert = require('assert');
 
 // escreva sum abaixo
+const sum = (...numeros) => {
+  return numeros
+  .reduce((total, numero) => total + numero, 0)
+}
+console.log(sum(1, 3, 4)) // 8
 
 assert.strictEqual(sum(), 0);
 assert.strictEqual(sum(1), 1);
