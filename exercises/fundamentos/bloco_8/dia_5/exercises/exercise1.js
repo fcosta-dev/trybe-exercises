1df21c9a64aed308efa15024d3dc5cbdd92dfb6f
fgosuen <fgosuendacosta@gmail.com>
2021-06-25 15:23:05 -0300

    Finalizando primeiro exercício

---
+++ exercises/fundamentos/bloco_8/dia_5/exercises/exercise1.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+
+const rectangleArea = (width, height) => width * height;
+
+const rectangle1 = [1, 2];
+const rectangle2 = [3, 5];
+const rectangle3 = [6, 9];
+const rectangles = [rectangle1, rectangle2, rectangle3];
+
+rectangles.forEach((rectangle) => {
+  console.log(rectangle[0] * rectangle[1] )
+  assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
+});
+
