3907c7ef6c59ea4b5f9b710664be70fbeeb81cf5
fgosuen <fgosuendacosta@gmail.com>
2021-06-29 09:17:55 -0300

    Iniciando os exercícios

--- exercises/fundamentos/bloco_9/dia_2/notefetch.js
+++ exercises/fundamentos/bloco_9/dia_2/notefetch.js
@@ -1,22 +1,19 @@
 const fetch = require('node-fetch');
 
-function verifiedFetch(url) {
-  return new Promise((resolve, reject) => {
-    if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
-      fetch(url)
-        .then((r) => r.json())
-        .then((r) => resolve(r.value));
-    } else {
-      reject(new Error('endpoint não existe'));
-    }
-  });
+async function verifiedFetch(url) {
+  if (url === 'https://api.chucknorris.io/jokes/random?category=dev') {
+    return fetch(url)
+      .then((r) => r.json())
+      .then((r) => (r.value));
+  }
+  throw new Error('endpoint não existe');
 }
 
-function sendJokeToFriend(name) {
-  const message = verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
+async function sendJokeToFriend(name) {
+  const message = await verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev')
     .then((joke) => `Oi ${name}, ouve essa: ${joke}`)
     .catch((err) => err);
   console.log(message);
 }
 
-sendJokeToFriend("Anna");
+sendJokeToFriend("Anna"); // Oi Anna, ouve essa: Chuck Norris can write multi-threaded applications with a single thread.
