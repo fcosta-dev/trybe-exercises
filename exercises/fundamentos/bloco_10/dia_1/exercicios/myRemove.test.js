bd79eb302f92eb7c431bc272cfcdbc07adce4856
fgosuen <fgosuendacosta@gmail.com>
2021-07-06 09:41:56 -0300

    Resolução Exercícios 1 da Parte 1

---
+++ exercises/fundamentos/bloco_10/dia_1/exercicios/myRemove.test.js
@@ -0,0 +1,11 @@
+const myRemove = require('./myRemove')
+
+describe('Requisito 2', () => {
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2,4])
+  })
+
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4])
+  })
+})
--- exercises/fundamentos/bloco_10/dia_1/sum.test.js
+++
@@ -1,5 +0,0 @@
-const sum = (a, b) => a + b;
-
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
-});
