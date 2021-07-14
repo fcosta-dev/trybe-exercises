ad2b1f8f4bd87aa537dacc1746b4f4e1d7b6df5a
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-14 12:45:01 -0300

    Iniciando os exercícios

---
+++ exercises/fundamentos/bloco_10/dia_3/para fixar/exercicio01.test.js
@@ -0,0 +1,30 @@
+// 1 - Faça o mock da funcão subtrair e teste sua chamada.
+const math = require("./math")
+
+describe('Exercicio 01 - Faça o mock da funcão subtrair e teste sua chamada.', () => {
+  test("testar chamada da função subtrair", () => {
+    math.subtrair = jest.fn();
+
+    math.subtrair();
+    expect(math.subtrair).toHaveBeenCalled();
+  });
+})
+
+// 2 - Faça o mock da função multiplicar e implemente como retorno padrão o valor '10'. Teste a chamada e o retorno.
+describe('Exercicio 02 - Faça o mock da função multiplicar e implemente como retorno padrão o valor ‘10’. Teste a chamada e o retorno.', () => {
+  test("testar chamada da função multiplicar", () => {
+    math.multiplicar = jest.fn();
+  
+    math.multiplicar();
+    expect(math.multiplicar).toHaveBeenCalled();
+  });
+
+  test("testar retorno da função multiplicar com retorno de valor '10'", () => {
+    math.multiplicar = jest.fn().mockReturnValue(10);
+  
+    math.multiplicar();
+    expect(math.multiplicar()).toBe(10);
+  });
+})
+
+// 3 - Faça o mock da função somar e implemente uma função que recebe dois valores e retorna sua soma. Teste a chamada, o retorno e os parâmetros passados.
