c4f64e6369dd67315e9be62fcb817ceeac6576f4
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-15 19:45:46 -0300

    Iniciando exercício 06

---
+++ exercises/fundamentos/bloco_10/dia_3/exercicios/exercicio_06.test.js
@@ -0,0 +1,2 @@
+// 6 - Crie uma função que faça requisição para a api dog pictures . Mocke a requisição e crie dois testes. O primeiro deve interpretar que a requisição se resolveu e teve como valor "request sucess". O segundo deve interpretar que a requisição falhou e ter como valor "request failed". Crie todos os testes que achar necessário.
+
