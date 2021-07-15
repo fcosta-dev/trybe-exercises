d4d8c66a6cb6c7f48b1477d3a43b39a146cd3891
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-15 19:43:20 -0300

    Finalizando exercício 05

--- exercises/fundamentos/bloco_10/dia_3/exercicios/exercicio_05.test.js
+++ exercises/fundamentos/bloco_10/dia_3/exercicios/exercicio_05.test.js
@@ -4,14 +4,19 @@ const exercicio_04 = require("./exercicio_04");
 
 describe('Exercício 05', () => {
   test('repetir a implementação para a primeira função', () => {
-    exercicio_04.executaMaiuscula = jest.fn().mockImplementation(x => x.toLowerCase())
+    const eMaiuscula = jest
+      .spyOn(exercicio_04, 'executaMaiuscula')
+      .mockImplementation(x => x.toLowerCase())
+
+    expect(eMaiuscula('FERNANDO')).toBe('fernando');
+
+    expect(eMaiuscula).toHaveBeenCalled();
+    expect(eMaiuscula).toHaveBeenCalledTimes(1);
+    expect(eMaiuscula).toHaveBeenCalledWith('FERNANDO');
 
     exercicio_04.executaMaiuscula.mockRestore(); // Reseta o mock
 
     expect(exercicio_04.executaMaiuscula('fernando')).toBe('FERNANDO');
-    expect(exercicio_04.executaMaiuscula).toHaveBeenCalled();
-    expect(exercicio_04.executaMaiuscula).toHaveBeenCalledTimes(1);
-    expect(exercicio_04.executaMaiuscula).toHaveBeenCalledWith('fernando');
 
   })
 })
