8f3cac9d120dd9ee4712cd57be09e79742f6d6ed
Fernando Costa <fgosuendacosta@gmail.com>
2021-07-29 10:56:09 -0300

    Adiçao de algumas resoluçoes do requisito 01

--- exercises/front-end/bloco_13/dia_3/sd-012-project-movie-card-library-crud-master/src/App.js
+++ exercises/front-end/bloco_13/dia_3/sd-012-project-movie-card-library-crud-master/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>Movie Card Library CRUD</div>
+        {/* BrowserRouter serve apenas para acessar as funcionalidades do Router.
+        Não renderiza nenhum HTML */}
+        <BrowserRouter>
+          {/* Funciona como um switch do JS, são opções de rotas conforme for clicado,
+          vai ser renderizado */}
+          <Switch>
+            {/* Abaixo são opções de rotas a serem renderizadas conforme escolha do
+             usuário, com seus caminhos no path */}
+            <Route exact path="/" component={ MovieList } />
+            <Route path="/movies/:id" component={ MovieDetails } />
+            <Route path="/movies/new" component={ NewMovie } />
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+            {/* Se o switch não pegar nenhuma rota anterior, cai nesta e apresenta
+             o NotFound */}
+            <Route component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
