998de1527345ec2ab6723d3e13ee9fc042544756
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-02 08:49:12 -0300

    Finalizando Conteudo Introdução ao RTL

--- exercises/front-end/bloco_15/dia_1/testes-react/src/App.test.js
+++ exercises/front-end/bloco_15/dia_1/testes-react/src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('Verificando se existe o campo Email.', () => {
@@ -22,3 +22,21 @@ test('Verificando se existe dois botões', () => {
   const btn = getAllByRole('button');
   expect(btn.length).toBe(2);
 });
+
+test('Verificando se o botão e o campo email estão funcionando.', () => {
+  const { getByTestId, getByLabelText } = render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = getByTestId('id-send');
+  const inputEmail = getByLabelText('Email');
+  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+  fireEvent.click(btnSend);
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
+});
+
