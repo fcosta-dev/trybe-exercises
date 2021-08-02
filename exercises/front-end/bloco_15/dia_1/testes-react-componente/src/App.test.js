4e2ead8139ce28e91cd6458d3cea7136b37754fb
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-02 09:15:21 -0300

    Adaptando para testar um componente

--- exercises/front-end/bloco_15/dia_1/testes-react-componente/src/App.test.js
+++ exercises/front-end/bloco_15/dia_1/testes-react-componente/src/App.test.js
@@ -1,8 +1,41 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verificando se existe o campo Email.', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  const { getByTestId } = render(<App />);
+  const btn = getByTestId('id-send');
+  expect(btn).toBeInTheDocument();
+  expect(btn.type).toBe('button');
+  expect(btn).toHaveValue('Enviar');
+});
+
+test('Verificando se existe dois botões', () => {
+  const { getAllByRole } = render(<App />);
+  const btn = getAllByRole('button');
+  expect(btn.length).toBe(2);
+});
+
+test('Verificando se o botão e o campo email estão funcionando.', () => {
+  const { getByTestId, getByLabelText } = render(<App />);
+
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent('Valor:');
+
+  const btnSend = getByTestId('id-send');
+  const inputEmail = getByLabelText('Email');
+  fireEvent.change(inputEmail, { target: { value: EMAIL_USER } });
+  fireEvent.click(btnSend);
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
 });
