79bff74f63115abbb9afeee3ef7490f1d70ed7ea
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-02 08:40:54 -0300

    Testando

--- exercises/front-end/bloco_15/dia_1/testes-react/src/App.test.js
+++ exercises/front-end/bloco_15/dia_1/testes-react/src/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Verificando se existe o campo Email.', () => {
+  const { getByLabelText } = render(<App />);
+  const inputEmail = getByLabelText('Email');
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe('email');
+});
+
+test('Verificando se existe um botão de enviar', () => {
+  const { getByTestId } = render(<App />);
+  const btn = getByTestId('id-send');
+  expect(btn).toBeInTheDocument();
+  expect(btn.type).toBe('button');
+  expect(btn).toHaveValue('Enviar');
+});
+
+test('Verificando se existe dois botões', () => {
+  const { getAllByRole } = render(<App />);
+  const btn = getAllByRole('button');
+  expect(btn.length).toBe(2);
 });
