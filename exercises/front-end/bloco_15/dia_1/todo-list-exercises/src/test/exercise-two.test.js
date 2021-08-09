d9f24725b8d9929645f9aac85402c5ef2738b640
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-09 11:38:48 -0300

    Terminando primeira parte do exercício 02

--- exercises/front-end/bloco_15/dia_1/todo-list-exercises/src/test/exercise-two.test.js
+++ exercises/front-end/bloco_15/dia_1/todo-list-exercises/src/test/exercise-two.test.js
@@ -4,14 +4,24 @@ import App from '../App';
 import Item from '../Item';
 import { describe } from 'yargs';
 import { test } from 'picomatch';
+import { ExpectationFailed } from 'http-errors';
 
 // Limpa renderizações que foram montadas para os testes
 afterEach(cleanup)
 
 describe('Teste do campo input', () => {
   test('Testando a adição de vários itens à aplicação', () => {
-
+    // PRIMEIRA PARTE -> ACESSAR OS ELEMENTOS NA TELA
+    const listTodo
   })
 })
 
-describe
+describe('Teste do componente Item', () => {
+  test('Ao receber uma string como prop ela precisa aparecer na tela', () => {
+    // Desconstruo o getByText do componente Item passando a props "content" a string "Todo test".
+    const { getByText } = render(<Item content="testando passagem da props" />);
+
+    // Verifico se o texto "testando passagem da props" aparece renderizado na tela.
+    expect(getByText('testando passagem da props')).toBeInTheDocument();
+  })
+})
