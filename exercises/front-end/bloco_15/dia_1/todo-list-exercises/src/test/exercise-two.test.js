6688b9af11e62215adc835ffb45c044bdb4026a5
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-09 11:52:13 -0300

    Terminando exercício 02

--- exercises/front-end/bloco_15/dia_1/todo-list-exercises/src/test/exercise-two.test.js
+++ exercises/front-end/bloco_15/dia_1/todo-list-exercises/src/test/exercise-two.test.js
@@ -2,9 +2,6 @@ import React from 'react'
 import { render, fireEvent, cleanup } from '@testing-library/react'
 import App from '../App';
 import Item from '../Item';
-import { describe } from 'yargs';
-import { test } from 'picomatch';
-import { ExpectationFailed } from 'http-errors';
 
 // Limpa renderizações que foram montadas para os testes
 afterEach(cleanup)
@@ -12,7 +9,29 @@ afterEach(cleanup)
 describe('Teste do campo input', () => {
   test('Testando a adição de vários itens à aplicação', () => {
     // PRIMEIRA PARTE -> ACESSAR OS ELEMENTOS NA TELA
-    const listTodo
+    // Criando um array de tarefas
+    const arrayTarefas = ['Acordar', 'Escovar os dentes', 'Tomar café']
+    // Desconstruo o getByLabelText e o getByText do componente App
+    const { getByLabelText, getByText } = render(<App />)
+    // Busco algum elemento com o Label "Tarefa:", a linha abaixo vai pegar o input com esse label
+    const input = getByLabelText('Tarefa:');
+    const button = getByText('Adicionar');
+
+    // SEGUNDA PARTE -> INTERAGIR COM OS ELEMENTOS
+    // Percorro todo o array de Tarefas para simular digitação e click no botão adicionar
+    arrayTarefas.forEach((tarefa) => {
+      // Simulo o digitar a tarefa percorrida no ForEach no campo input de texto 
+      fireEvent.change(input, { target: { value: tarefa } });
+      // Simulo o click no botão para adicionar a tarefa acima percorrida pelo ForEach
+      fireEvent.click(button);
+    })
+
+    // TERCEIRA PARTE -> FAZ O TESTE
+    // Percorro mais uma vez o array de Tarefas para verificar se cada tarefa está renderizada
+    arrayTarefas.forEach((tarefa) => {
+      // Faço o teste analizando se a tarefa está renderizada no documento 
+      expect(getByText(tarefa)).toBeInTheDocument();
+    })
   });
 })
 
