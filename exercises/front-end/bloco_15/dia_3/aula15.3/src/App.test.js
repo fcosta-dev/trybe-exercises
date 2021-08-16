323f870620bb4e42e1bcc4d6c33a6591cddc1594
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-16 08:16:33 -0300

    Executando testes

--- exercises/front-end/bloco_15/dia_3/aula15.3/src/App.test.js
+++ exercises/front-end/bloco_15/dia_3/aula15.3/src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('teste da aplicação toda', () => {
+  it('deve renderizar o componente App', () => {
+    const { getByText } = renderWithRouter(<App />);
+    const home = getByText(/Você está na página Início/);
+    expect(home).toBeInTheDocument();
+  });
+
+  it('deve renderizar o componente Sobre', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    fireEvent.click(getByText(/Sobre/i));
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/about');
+    const aboutAll = getByText(/Você está na página Sobre/);
+    expect(aboutAll).toBeInTheDocument();
+  });
+
+  it('deve testar um caminho não existente e a renderização do Not Found', () => {
+    const { getByText, history } = renderWithRouter(<App />);
+    history.push('/pagina/que-nao-existe/');
+    const noMatch = getByText(/Página não encontrada/i);
+    expect(noMatch).toBeInTheDocument();
+  });
+}
