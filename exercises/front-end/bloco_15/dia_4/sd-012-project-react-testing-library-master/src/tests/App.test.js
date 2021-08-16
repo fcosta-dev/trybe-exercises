4a9e2fad19ff1c563d9a853060ccc6fafc5e5cdd
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-16 08:39:56 -0300

    Iniciando projeto

--- exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/App.test.js
+++ exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/App.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+// npx stryker run ./stryker/PokemonDetails.conf.json para confirmar que os testes foram realizados em 100%
+
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
+describe('Requisito 01 - Teste do componente .js', () => {
+  it('O primeiro link deve possuir o texto Home', () => {
+
+  });
+
+  it('O primeiro link deve possuir o texto Home', () => {
+
+  });
+
+  it('O primeiro link deve possuir o texto Home', () => {
+
+  });
+
+  it('O primeiro link deve possuir o texto Home', () => {
+
+  });
+
+
+})
