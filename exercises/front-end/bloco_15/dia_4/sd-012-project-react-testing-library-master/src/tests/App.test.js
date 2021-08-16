0976fa74ea657629f366110e248b14563f2ee621
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-16 08:53:16 -0300

    Adicionando testes do requisito 01

--- exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/App.test.js
+++ exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/App.test.js
@@ -4,22 +4,29 @@ import React from 'react';
 import { screen, fireEvent } from '@testing-library/react';
 import renderWithRouter from './renderWithRouter';
 import App from '../App';
+
 describe('Requisito 01 - Teste do componente .js', () => {
-  it('O primeiro link deve possuir o texto Home', () => {
-
+  test('O primeiro link deve possuir o texto Home', () => {
+    // Como a função render normal da RTL não dá suporte ao router precisamos usar a função renderWithRouter. Ela pode ser muito útil e usa o createMemoryHistory para embutir no seu componente renderizado a lógica de histórico de navegação , para uso nos teste.
+    renderWithRouter(<App />);
+    // getByRole: Isso pode ser usado para consultar todos os elementos expostos na árvore de acessibilidade
+    // Com a name opção, você pode filtrar os elementos retornados por seus nomes acessíveis
+    // Ou seja, usando o screen verificamos se na tela, possui um "link" com o name "home", se tiver guarda ele na variável home.
+    const home = screen.getByRole('link', { name: /home/i });
+    // Testo se o conteúdo da variável home está no documento
+    expect(home).toBeInTheDocument();
   });
 
-  it('O primeiro link deve possuir o texto Home', () => {
-
+  test('O segundo link deve possuir o texto About', () => {
+    // Renderizo na tela as rotas do componente App
+    renderWithRouter(<App />);
+    // Verifico na tela, se há um elemento do tipo link com o name "about", e se tiver coloca o elemento na variável about.
+    const about = screen.getByRole('link', { name: /about/i });
+    // Testo que espero que a variável about esteja no documento
+    expect(about).toBeInTheDocument();
   });
 
-  it('O primeiro link deve possuir o texto Home', () => {
+  test('O terceiro link deve possuir o texto Favorite Pokémons', () => {
 
   });
-
-  it('O primeiro link deve possuir o texto Home', () => {
-
-  });
-
-
-})
+});
