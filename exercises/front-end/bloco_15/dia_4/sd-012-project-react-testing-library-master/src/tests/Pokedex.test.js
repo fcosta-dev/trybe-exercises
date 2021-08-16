4ad287838d833416cbc1cfabf18e410902fcfa3c
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-16 16:00:13 -0300

    Avançando no requisito 05

--- exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/Pokedex.test.js
+++ exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/Pokedex.test.js
@@ -6,14 +6,24 @@ import renderWithRouter from './renderWithRouter';
 
 import pokemons from '../data';
 
+// Devido erros de lint teve que ser criado estas constantes
+const nameTestId = 'pokemon-name';
+const typeTestId = 'pokemon-type';
+const typeButtonTestId = 'pokemon-type-button';
+
+// Cria na constante uniquePokemonTypes um array de tipos de pokemon, tirando os valores repetidos.
+const uniquePokemonTypes = [...new Set(pokemonTypes)];
+
 describe('Requisito 05 - Teste o componente <Pokedex.js />', () => {
-  test('Testa se página contém um h2 com o texto Encountered pokémons', () => {
-    // Renderizo na tela as rotas do componente App.
-    renderWithRouter(<App />);
-    // Busca na tela se há um heading(h1,h2,h3...) com name "Encountered pokémons"
-    const h2 = screen.getByRole('heading', { name: /Encountered pokémons/i });
-    // Verifica se o elemento que está na h2 está no documento
-    expect(h2).toBeInTheDocument();
+  describe('Testa o heading da página', () => {
+    test('Testa se página contém um h2 com o texto Encountered pokémons', () => {
+      // Renderizo na tela as rotas do componente App.
+      renderWithRouter(<App />);
+      // Busca na tela se há um heading(h1,h2,h3...) com name "Encountered pokémons"
+      const h2 = screen.getByRole('heading', { name: /Encountered pokémons/i });
+      // Verifica se o elemento que está na h2 está no documento
+      expect(h2).toBeInTheDocument();
+    });
   });
 
   describe('Testa se exibe o próximo Pokémon se Próximo pokémon é clicado', () => {
@@ -35,13 +45,12 @@ describe('Requisito 05 - Teste o componente <Pokedex.js />', () => {
         // Através da importação de dados de pokemons na "pokemons" faço um foreach para ler cada um dos pokemons
         pokemons.forEach((pokemon) => {
           // através do data-test-id verifica se há um elemento pokemon-name
-          const pokemonName = screen.getByTestId('pokemon-name');
+          const pokemonName = screen.getByTestId(nameTestId);
           // Testa se o elemento que está na variável acima tem o name do elemento pokemon do foreach. Ou seja, verifica se é o mesmo pokemon.
           expect(pokemonName).toHaveTextContent(pokemon.name);
           // Simula o click no botão de próximo pokemon
           fireEvent.click(nextPkmBtn);
         });
-
       });
 
     test('O primeiro Pokémon deve ser mostrado se estiver no último Pokémon da lista',
@@ -51,18 +60,83 @@ describe('Requisito 05 - Teste o componente <Pokedex.js />', () => {
         const nextPkmBtn = screen.getByRole('button', { name: /Próximo pokémon/i });
         // Percorro a lista de pokemons
         pokemons.forEach(() => {
+          // Simulo o click no botão de próximo pokemon
           fireEvent.click(nextPkmBtn);
         });
+        // através do data-test-id pega o elemento com o name abaixo
         const firstPokemon = screen.getByTestId(nameTestId);
+        // Testa se o pokemon pego acima tem a mesma descrição do primeiro pokemon a ser pego no foreach acima
         expect(firstPokemon).toHaveTextContent(pokemons[0].name);
       });
   });
 
-  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-
+  describe('Testa se é mostrado apenas um Pokémon por vez', () => {
+    test('Testa se é mostrado apenas um Pokémon por vez', () => {
+      // Renderizo na tela as rotas do componente App.
+      renderWithRouter(<App />);
+      // Busca pelo data-test-id se o pokemon name é trazido uma vez
+      const onlyOnePokemon = screen.getAllByTestId('pokemon-name');
+      // Testa se o tamanho da variável é d e1
+      expect(onlyOnePokemon).toHaveLength(1);
+    });
   });
 
-  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
-
+  describe('Testa se a Pokédex tem os botões de filtro', () => {
+    test('Deve existir um botão para cada tipo de Pokémon, sem repetição', () => {
+      // Renderizo na tela as rotas do componente App.
+      renderWithRouter(<App />);
+      // Com o array de tipos de pokemon, sem estarem repetidos, percorro ele todo com o foreach
+      uniquePokemonTypes.forEach((pokemonType, index) => {
+        // Testo se na tela, com o data-test-id, há um elemento com o texto de um tipo de pokemon. OU seja, verifica se para todos os tipos no array há um botão renderizado.
+        expect(screen.getAllByTestId(typeButtonTestId)[index])
+          .toHaveTextContent(pokemonType);
+      });
+    });
+    test('Se selecionado Tipo, a Pokédex deve circular apenas por pokémons daquele tipo',
+      () => {
+        // Renderizo na tela as rotas do componente App.
+        renderWithRouter(<App />);
+        uniquePokemonTypes.forEach((pokemonType, index) => {
+          fireEvent.click(screen.getAllByTestId(typeButtonTestId)[index]);
+          fireEvent.click(screen.getByTestId('next-pokemon'));
+          expect(screen.getByTestId(typeTestId)).toHaveTextContent(pokemonType);
+        });
+      });
+    test('O texto do botão deve corresponder ao nome do tipo, ex. Psychic',
+      () => {
+        // Renderizo na tela as rotas do componente App.
+        renderWithRouter(<App />);
+        uniquePokemonTypes.forEach((pokemonType, index) => {
+          fireEvent.click(screen.getAllByTestId(typeButtonTestId)[index]);
+          expect(screen.getByTestId(typeTestId)).toHaveTextContent(pokemonType);
+        });
+      });
+    test('O botão All precisa estar sempre visível', () => {
+      // Renderizo na tela as rotas do componente App.
+      renderWithRouter(<App />);
+      uniquePokemonTypes.forEach((pokemonType, index) => {
+        fireEvent.click(screen.getAllByTestId(typeButtonTestId)[index]);
+        fireEvent.click(screen.getByTestId('next-pokemon'));
+        expect(screen.getByRole('button', { name: /all/i })).toBeInTheDocument();
+      });
+    });
+  });
+  describe('Testa se a Pokédex contém um botão para resetar o filtro', () => {
+    test('O texto do botão deve ser All', () => {
+      // Renderizo na tela as rotas do componente App.
+      renderWithRouter(<App />);
+      expect(screen.getByRole('button', { name: /all/i })).toHaveTextContent('All');
+    });
+    test('A Pokedéx deverá mostrar os Pokémons sem filtros quando All for clicado',
+      () => {
+        // Renderizo na tela as rotas do componente App.
+        renderWithRouter(<App />);
+        const nextPkmBtn = screen.getByRole('button', { name: /Próximo pokémon/i });
+        uniquePokemonTypes.forEach((pokemonType, index) => {
+          fireEvent.click(screen.getAllByTestId(typeButtonTestId)[index]);
+          fireEvent.click(screen.getByRole('button', { name: /all/i }));
+          checkAllPokemons(nextPkmBtn);
+        });
+      });
   });
 });
