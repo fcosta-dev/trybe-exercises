fd96c3dfbca728375889eef0e3108f87f672288c
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-16 15:36:04 -0300

    Continuando requisito 05

--- exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/Pokedex.test.js
+++ exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/Pokedex.test.js
@@ -15,15 +15,48 @@ describe('Requisito 05 - Teste o componente <Pokedex.js />', () => {
     // Verifica se o elemento que está na h2 está no documento
     expect(h2).toBeInTheDocument();
   });
-  
+
   describe('Testa se exibe o próximo Pokémon se Próximo pokémon é clicado', () => {
     test('O botão deve conter o texto Próximo pokémon', () => {
+      // Renderizo na tela as rotas do componente App.
       renderWithRouter(<App />);
+      // Busca elemento button com name "Próximo pokémon"
       const nextPkmBtn = screen.getByRole('button', { name: /Próximo pokémon/i });
+      // Testa se o elemento acima buscado está no documento
       expect(nextPkmBtn).toBeInTheDocument();
     });
+
     test('Os próximos Pokémons são mostrados, um a um, ao clicar sucessivamente no botão',
       () => {
+        // Renderizo na tela as rotas do componente App.
+        renderWithRouter(<App />);
+        // Busca elemento button com name "Próximo pokémon"
+        const nextPkmBtn = screen.getByRole('button', { name: /Próximo pokémon/i });
+        // Através da importação de dados de pokemons na "pokemons" faço um foreach para ler cada um dos pokemons
+        pokemons.forEach((pokemon) => {
+          // através do data-test-id verifica se há um elemento pokemon-name
+          const pokemonName = screen.getByTestId('pokemon-name');
+          // Testa se o elemento que está na variável acima tem o name do elemento pokemon do foreach. Ou seja, verifica se é o mesmo pokemon.
+          expect(pokemonName).toHaveTextContent(pokemon.name);
+          // Simula o click no botão de próximo pokemon
+          fireEvent.click(nextPkmBtn);
+        });
+
+      });
+
+    test('O primeiro Pokémon deve ser mostrado se estiver no último Pokémon da lista',
+      () => {
+        // Renderizo na tela as rotas do componente App.
+        renderWithRouter(<App />);
+        const nextPkmBtn = screen.getByRole('button', { name: /Próximo pokémon/i });
+        // Percorro a lista de pokemons
+        pokemons.forEach(() => {
+          fireEvent.click(nextPkmBtn);
+        });
+        const firstPokemon = screen.getByTestId(nameTestId);
+        expect(firstPokemon).toHaveTextContent(pokemons[0].name);
+      });
+  });
 
   test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
 
