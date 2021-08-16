58669497bc006dcc2b44deb35abdcb2923a18b54
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-16 15:16:51 -0300

    Iniciando requisito 04

--- exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/NotFound.test.js
+++ exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/NotFound.test.js
@@ -1,1 +1,33 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+
+import NotFound from '../components/NotFound';
+import renderWithRouter from './renderWithRouter';
+
+describe('Requisito 04 - Teste o componente NotFound.js', () => {
+  test('Testa se página contém um h2 com o texto Page requested not found 😭', () => {
+    // Renderizo na tela as rotas do componente NotFound
+    renderWithRouter(<NotFound />);
+    // getByRole um elemento 'heading'(h1/h2/h3/h4...) busca um título, ou qualquer nível de título, com o name de "Page requested not found".
+    const message = screen.getByRole('heading', { name: /Page requested not found/i });
+    // Busca um elemento img com o name "Crying emoji"
+    const cryingEmoji = screen.getByRole('img', { name: /Crying emoji/i });
+    // Testa se o conteúdo da variável message está no documento
+    expect(message).toBeInTheDocument();
+    // Testa se o conteúdo da variável cryingEmoji está no documento
+    expect(cryingEmoji).toBeInTheDocument();
+  });
+  test('Testa se página mostra uma imagem específica', () => {
+    // Renderizo na tela as rotas do componente NotFound
+    renderWithRouter(<NotFound />);
+    // Guarda texto na variável "message"
+    const message = 'Pikachu crying because the page requested was not found';
+    // getByAltText: Se o seu elemento é aquele que suporta alttexto ( img, areae input), então você pode usar isso para encontrar esse elemento.
+    // Busca um elemento img através do getByAltText com o conteúdo da variável message
+    const img = screen.getByAltText(message);
+    // Guarda no imgURL o link da imagem definida no README
+    const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    // Verifica se na variável img, se o src dela é igual ao link que está guardado na variável imgURL
+    expect(img.src).toEqual(imgURL);
+  });
+});
--- exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/Pokedex.test.js
+++ exercises/front-end/bloco_15/dia_4/sd-012-project-react-testing-library-master/src/tests/Pokedex.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+import pokemons from '../data';
+
+describe('Requisito 05 - Teste o componente <Pokedex.js />', () => {
+  test('Testa se página contém um h2 com o texto Encountered pokémons', () => {
+    // Renderizo na tela as rotas do componente App.
+    renderWithRouter(<App />);
+    // Busca na tela se há um heading(h1,h2,h3...) com name "Encountered pokémons"
+    const h2 = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    // Verifica se o elemento que está na h2 está no documento
+    expect(h2).toBeInTheDocument();
+  });
+  
+  describe('Testa se exibe o próximo Pokémon se Próximo pokémon é clicado', () => {
+    test('O botão deve conter o texto Próximo pokémon', () => {
+      renderWithRouter(<App />);
+      const nextPkmBtn = screen.getByRole('button', { name: /Próximo pokémon/i });
+      expect(nextPkmBtn).toBeInTheDocument();
+    });
+    test('Os próximos Pokémons são mostrados, um a um, ao clicar sucessivamente no botão',
+      () => {
+
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+
+  });
+
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+
+  });
+});
