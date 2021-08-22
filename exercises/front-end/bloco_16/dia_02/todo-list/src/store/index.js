64a153547c6c231f6b6bf96a5ba73d29da2fe770
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-22 16:25:30 -0300

    Atualizando

--- exercises/front-end/bloco_16/dia_02/todo-list/src/store/index.js
+++
@@ -1,8 +0,0 @@
-// src/store/index.js
-import { createStore } from 'redux';
-
-import rootReducer from './reducers';
-
-const store = createStore(rootReducer);
-
-export default store;
--- exercises/front-end/bloco_16/dia_02/todo-list/src/store/reducers/index.js
+++
@@ -1,8 +0,0 @@
-// src/store/reducers/index.js
-import { combineReducers } from 'redux';
-
-import todo from './todo';
-
-export default combineReducers({
-  todo});
-  
