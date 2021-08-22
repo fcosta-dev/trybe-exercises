bf08e036eb2ea45858c7d9260da56874025c3922
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-22 14:19:50 -0300

    Aplicando alterações no exercicio 02

--- exercises/front-end/bloco_16/dia_02/exercise-react-with-redux-intro-master-02/src/Cars.js
+++ exercises/front-end/bloco_16/dia_02/exercise-react-with-redux-intro-master-02/src/Cars.js
@@ -1,25 +1,65 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import { connect } from 'react-redux';
+
+// Importando imagens dos carros
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
 
-export default function Cars({
-  redCar, blueCar, yellowCar, moveCar,
-}) {
+// Importando função action moveCar do arquivo actionCreators
+import { moveCar } from './redux/actionCreators';
+
+// É um componente funcional
+function Cars({ redCar, blueCar, yellowCar, moveCar }) {
   return (
     <div>
       <div>
-        <img className={redCar ? 'car-right' : 'car-left'} src={carRed} alt="red car" />
-        <button onClick={() => moveCar('red', !redCar)} type="button">move</button>
+        <img
+          className={redCar ? 'car-right' : 'car-left'}
+          src={carRed}
+          alt="red car"
+        />
+        <button onClick={() => moveCar('red', !redCar)} type="button">
+          Move
+        </button>
       </div>
       <div>
-        <img className={blueCar ? 'car-right' : 'car-left'} src={carBlue} alt="blue car" />
-        <button onClick={() => moveCar('blue', !blueCar)} type="button">move</button>
+        <img
+          className={blueCar ? 'car-right' : 'car-left'}
+          src={carBlue}
+          alt="blue car"
+        />
+        <button onClick={() => moveCar('blue', !blueCar)} type="button">
+          Move
+        </button>
       </div>
       <div>
-        <img className={yellowCar ? 'car-right' : 'car-left'} src={carYellow} alt="yellow car" />
-        <button onClick={() => moveCar('yellow', !yellowCar)} type="button">move</button>
+        <img
+          className={yellowCar ? 'car-right' : 'car-left'}
+          src={carYellow}
+          alt="yellow car"
+        />
+        <button onClick={() => moveCar('yellow', !yellowCar)} type="button">
+          Move
+        </button>
       </div>
     </div>
   );
 }
+
+Cars.propTypes = {
+  moveCar: PropTypes.func.isRequired,
+  blueCar: PropTypes.bool.isRequired,
+  redCar: PropTypes.bool.isRequired,
+  yellowCar: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  redCar: state.cars.red,
+  blueCar: state.cars.blue,
+  yellowCar: state.cars.yellow});
+
+const mapDispatchToProps = { moveCar };
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cars);
