8b9bd6395cac4ce6c1bfd462c2068db1221055b5
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-22 16:11:11 -0300

    Atualizando

--- exercises/front-end/bloco_16/dia_02/todo-list/src/index.js
+++ exercises/front-end/bloco_16/dia_02/todo-list/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './store/reducers/todo'
 
-ReactDOM.render(
+const store = createStore(reducer)
+
+render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
