73af500ec7ff3bcfb303ef9d2d663f1aca331bb5
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-23 09:41:45 -0300

    Comentários monitoria

--- exercises/front-end/bloco_16/dia_02/exercise-react-with-redux-intro-master-03/src/redux/trafficReducer.js
+++ exercises/front-end/bloco_16/dia_02/exercise-react-with-redux-intro-master-03/src/redux/trafficReducer.js
@@ -10,7 +10,10 @@ function trafficReducer(state = initialState, action) {
   switch (action.type) {
     case CHANGE_SIGNAL:
       return {
+        // Retorna o state inicial ou state passado como parametro, com o spread operator
         ...state,
+        // Pego todos os sinais que estavam anteriormente, e faço a alteração
+        // Como está sendo colocado uma chave igual, essa chave vai ter valor alterado
         signal: { ...state.signal, color: action.payload }
       };
     default:
