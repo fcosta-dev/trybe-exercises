68036fa1b83c46085fef8874c6ba1790dce069db
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-24 19:59:43 -0300

    Iniciando exercício

--- exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/actions/index.js
+++ exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/actions/index.js
@@ -2,6 +2,10 @@ export const addRegister = value => ({
   type: 'ADD_REGISTER', data: value
 });
 
+export const deleteRegister = value => ({
+  type: 'DELETE_REGISTER', value
+});
+
 export const login = value => ({
   type: 'LOGIN', value
 });
