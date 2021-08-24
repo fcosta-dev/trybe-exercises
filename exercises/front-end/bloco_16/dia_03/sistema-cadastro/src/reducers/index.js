031541bcc521481474b59b04b73b1a7d467b273f
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-24 19:50:03 -0300

    Iniciando exercício

---
+++ exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import loginReducer from './login';
+import registerReducer from './register';
+
+const rootReducers = combineReducers({
+  registerReducer,
+  loginReducer
+});
+
+export default rootReducers;
