5f95189bf4a22a08e12f78df4975c08727b513fb
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-25 07:10:12 -0300

    Fazendo CSS

--- exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/pages/Home.js
+++ exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/pages/Home.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-class Home extends React.Component {
+class Home extends Component {
   render() {
     return (
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '30vh'}}>
-        Bem-vindo ao sistema de cadastramento de clientes
-        <br />
-        <br />
-        <Link to="/Login">Faça seu Login</Link>
-      </div>
+      <>
+        <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '30vh'}}>
+          Bem-vindo ao sistema de cadastramento de clientes
+        </div>
+        <div  style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
+          <br />
+          <br />
+          <br />
+          {/* Realiza o apontamento para /login */}
+          <Link to="/Login">Faça seu Login</Link>
+        </div>
+      </>
     );
   }
 }
