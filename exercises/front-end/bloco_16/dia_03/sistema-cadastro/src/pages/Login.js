7de7d91ff24103a3918f0951736170fefa6d0607
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-25 07:15:56 -0300

    Corrigindo CSS do Login

--- exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/pages/Login.js
+++ exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/pages/Login.js
@@ -6,25 +6,34 @@ import { Link } from 'react-router-dom';
 class Login extends React.Component {
   constructor(props) {
     super(props);
+
+    // State inicial
     this.state = {
       email: '',
       password: '',
     };
   }
+
   render() {
     const { email, password } = this.state;
     return (
       <div>
-        <div>
+        <div 
+          className="div_inputs"
+          style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '30vh', flexDirection: 'column' }}
+          >
+          <h3>Login</h3> 
           <input
             type="text"
             onChange={event => this.setState({ email: event.target.value })}
             placeholder="email"
+            style={ { height:'25px' } }
           />
           <input
             type="password"
             onChange={event => this.setState({ password: event.target.value })}
             placeholder="senha"
+            style={ { height:'25px' } }
           />
         </div>
         <Link
@@ -37,7 +46,8 @@ class Login extends React.Component {
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  login: e => dispatch(login(e))});
+const mapDispatchToProps = (dispatch) => ({
+  login: event => dispatch(login(event))});
 
+// Por não ter necessidade de leitura da Store, temos apenas o Disparo para gravar algo na Store
 export default connect(null, mapDispatchToProps)(Login);
