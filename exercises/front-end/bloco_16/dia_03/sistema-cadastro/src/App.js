acb0eebde21642da89cd4a429d8d72c696efeb90
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-25 08:22:21 -0300

    Adicionando comentários explicativos

--- exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/App.js
+++ exercises/front-end/bloco_16/dia_03/sistema-cadastro/src/App.js
@@ -8,14 +8,14 @@ import Clients from './pages/Clients';
 class App extends React.Component {
   render() {
     return (
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/" component={ Home } />
-            <Route path="/login" component={ Login } />
-            <Route path="/register" component={ Register } />
-            <Route path="/clients" component={ Clients } />
-          </Switch>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+          <Route path="/login" component={ Login } />
+          <Route path="/register" component={ Register } />
+          <Route path="/clients" component={ Clients } />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
