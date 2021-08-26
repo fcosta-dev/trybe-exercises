29506c4b4922480d6cf70f437829af5e41af46a3
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-26 13:51:41 -0300

    Finalizando

--- exercises/front-end/bloco_16/dia_04/reddit/src/App.js
+++ exercises/front-end/bloco_16/dia_04/reddit/src/App.js
@@ -1,25 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { selectSubreddit, fetchPostsIfNeeded, refreshSubreddit } from './actions';
+import Posts from './components/Posts';
+import Selector from './components/Selector';
+
+class App extends Component {
+  componentDidMount() {
+    const { dispatch, selectedSubreddit } = this.props;
+    dispatch(fetchPostsIfNeeded(selectedSubreddit));
+  }
+
+  componentDidUpdate(prevProps) {
+    const { props } = this;
+
+    if (prevProps.selectedSubreddit !== props.selectedSubreddit) {
+      const { dispatch, selectedSubreddit } = props;
+      dispatch(fetchPostsIfNeeded(selectedSubreddit));
+    }
+  }
+
+  selectSubreddit(nextSubreddit) {
+    const { dispatch } = this.props;
+    dispatch(selectSubreddit(nextSubreddit));
+  }
+
+  handleRefreshClick(event) {
+    event.preventDefault();
+
+    const { dispatch, selectedSubreddit } = this.props;
+    dispatch(refreshSubreddit(selectedSubreddit));
+    dispatch(fetchPostsIfNeeded(selectedSubreddit));
+  }
+
+  renderLastUpdatedAt() {
+    const { lastUpdated } = this.props;
+
+    return (
+      <span>
+        {`Last updated at ${new Date(lastUpdated).toLocaleTimeString()}.`}
+      </span>
+    );
+  }
+
+  renderRefreshButton() {
+    const { isFetching } = this.props;
+
+    return (
+      <button
+        type="button"
+        onClick={(event) => this.handleRefreshClick(event)}
+        disabled={isFetching}
+      >
+        Refresh
+      </button>
+    );
+  }
+
+  render() {
+    const {
+      availableSubreddits,
+      selectedSubreddit,
+      posts = [],
+      isFetching,
+      lastUpdated,
+    } = this.props;
+
+    const isEmpty = posts.length === 0;
+
+    return (
+      <div>
+        <Selector
+          value={selectedSubreddit}
+          onChange={(nextSubreddit) => this.selectSubreddit(nextSubreddit)}
+          options={availableSubreddits}
+        />
+        <div>
+          {lastUpdated && this.renderLastUpdatedAt()}
+          {this.renderRefreshButton()}
+        </div>
+        {isFetching && <h2>Loading...</h2>}
+        {!isFetching && isEmpty && <h2>Empty.</h2>}
+        {!isFetching && !isEmpty && <Posts posts={posts} />}
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  const { selectedSubreddit, postsBySubreddit } = state;
+  const {
+    isFetching,
+    lastUpdated,
+    items: posts,
+  } = postsBySubreddit[selectedSubreddit];
+
+  return {
+    selectedSubreddit,
+    posts,
+    isFetching,
+    lastUpdated,
+    availableSubreddits: Object.keys(postsBySubreddit),
+  };
+};
+
+App.propTypes = {
+  availableSubreddits: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  dispatch: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  lastUpdated: PropTypes.number,
+  posts: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  })),
+  selectedSubreddit: PropTypes.string.isRequired,
+};
+
+App.defaultProps = {
+  lastUpdated: null,
+  posts: [],
+};
+
+export default connect(mapStateToProps)(App);
---
+++ exercises/front-end/bloco_16/dia_04/reddit/src/reducers/selectedSubreddit.js
@@ -0,0 +1,16 @@
+// src/reducers/selectedSubreddit.js
+
+import {
+  SELECT_SUBREDDIT,
+} from '../actions';
+
+const selectedSubreddit = (state = 'reactjs', action) => {
+  switch (action.type) {
+    case SELECT_SUBREDDIT:
+      return action.subreddit;
+    default:
+      return state;
+  }
+};
+
+export default selectedSubreddit;
