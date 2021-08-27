120b40ab79f5c628aaf9588c36cfbcff9d70f9c0
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-27 01:15:16 -0300

    Reiniciando

--- exercises/front-end/bloco_16/dia_06/sd-012-project-trybewallet-master/src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-// import user from './user';
-// import wallet from './wallet';
-
-// Configure os seus reducers.
-// ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
