42e6a0c32e09b715b4421e6766ba787c97e9f0ea
Fernando Costa <fgosuendacosta@gmail.com>
2021-08-27 01:28:23 -0300

    Adicionando router dom

--- exercises/front-end/bloco_16/dia_06/sd-012-project-trybewallet-master/src/pages/Login.js
+++ exercises/front-end/bloco_16/dia_06/sd-012-project-trybewallet-master/src/pages/Login.js
@@ -1,9 +1,62 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { login } from '../actions';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // State inicial
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password } = this.state;
+    return (
+      <div>
+        <div
+          className="div_inputs"
+          style={ {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '30vh',
+            flexDirection: 'column',
+          } }
+        >
+          <h3>Login</h3>
+          <input
+            type="text"
+            onChange={ (event) => this.setState({ email: event.target.value }) }
+            placeholder="email"
+            style={ { height: '25px' } }
+          />
+          <input
+            type="password"
+            onChange={ (event) => this.setState({ password: event.target.value }) }
+            placeholder="senha"
+            style={ { height: '25px' } }
+          />
+          <br />
+          <Link
+            to="/clients"
+            // Chamo o login que está na props e disparo os panametros email e password
+            onClick={ () => this.props.login({ email, password }) }
+          >
+            Entre
+          </Link>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Login;
+// const mapDispatchToProps = (dispatch) => ({
+//   login: event => dispatch(login(event))});
+
+// Por não ter necessidade de leitura da Store, temos apenas o Disparo para gravar algo na Store
+export default connect(null, null)(Login);
