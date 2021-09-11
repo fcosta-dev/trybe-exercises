3c8287af360e46a8a1214939b947bb381d5a3d5c
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-11 10:13:16 -0300

    Iniciando exercicio de fixação

--- exercises/front-end/bloco_18/dia_03/react-hooks-10segs/src/App.js
+++ exercises/front-end/bloco_18/dia_03/react-hooks-10segs/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// Faz a importação do componente funcional useTimer 
+import useTimer from './useTimer';
+
+const App = () => {
+  const { timer, randomNumber, isMultiple } = useTimer();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {timer}
+      <br />
+      {randomNumber}
+      <br />
+      {isMultiple ? 'Acerto' : ''}
     </div>
   );
-}
+};
 
 export default App;
