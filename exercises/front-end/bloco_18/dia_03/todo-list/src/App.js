3f9e8e6ee7a3b15b86f898e5b815d3f937f4a9f4
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-11 16:00:31 -0300

    Iniciando hooks customizados

--- exercises/front-end/bloco_18/dia_03/todo-list/src/App.js
+++ exercises/front-end/bloco_18/dia_03/todo-list/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
+import useArray from './hooks/useArray';
 
 function App() {
+  // Cria o getter e setter do New Input com valor inicial em branco
+  const [newInput, setNewInput] = useState('');
+  // Lista de tarefas
+  const { addTodos, todos } = useArray();
+
+  // Seta o novo valor de input(setNewInput) como o valor que está no target
+  // Função irá rodar no onChange do "Add a task:"
+  const handleInput = ({ target }) => {
+    setNewInput(target.value);
+  };
+
+  // Quando o botão Add for clicado..
+  const handleClick = () => {
+    // Chama a fução addTodos do hooks passando a nova tarefa como parametro
+    // Isso vai concatena-la às demais tarefas gravas no array  
+    addTodos(newInput);
+    // Seta que o setNewInput agora é vazio
+    setNewInput('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label htmlFor="task-input">
+        Add a task:
+        <input
+          id="task-input"
+          placeholder="task"
+          name="newItem"
+          value={ newInput }
+          onChange={ handleInput }
+        />
+      </label>
+      <button type="button" onClick={ () => handleClick() }>Add</button>
+      {/* Chama o componente TodoList passando a tasks como props */}
+      <TodoList tasks={ todos } />
     </div>
   );
 }
