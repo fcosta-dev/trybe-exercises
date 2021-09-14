edf18c59a3c1dcd9f3656ff227721ac427613a01
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-14 18:27:15 -0300

    Iniciando projeto

--- exercises/front-end/bloco_19/sd-012-project-recipes-app-context/src/context/Context.js
+++
@@ -1,7 +0,0 @@
-// Desconstrói o context do  react
-import { createContext } from 'react';
-
-const Context = createContext();
-
-// Exporta o Context para ser usado no context/Provider.js
-export default Context;
