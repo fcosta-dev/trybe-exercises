2fe91431f6248e22900beb319c88bd7e39197eb6
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-14 08:25:41 -0300

    Criando a Estrutura Inicial

--- exercises/front-end/bloco_19/sd-012-project-recipes-app-context/src/App.js
+++ exercises/front-end/bloco_19/sd-012-project-recipes-app-context/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+// Importa o hook useContext
+import React, { useContext } from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Importar o arquivo Context criado
+import Context from './context/Context';
+
 function App() {
+  const { teste } = useContext(Context);
+
   return (
     <div className="meals">
       <span className="logo">TRYBE</span>
--- exercises/front-end/bloco_19/sd-012-project-recipes-app-context/src/index.js
+++ exercises/front-end/bloco_19/sd-012-project-recipes-app-context/src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './store';
+
+// Importa o Provider do /context/Provider
+import Provider from './context/Provider';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root'),
+  <Provider>
+    <App />
+  </Provider>, document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
