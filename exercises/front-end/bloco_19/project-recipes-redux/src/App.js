d891ca31e06e1aebe8ddcaea4899ccddcc779506
Fernando Costa <fgosuendacosta@gmail.com>
2021-09-18 10:22:59 -0300

    Requisito 2 a 8 - Tela de Login

--- exercises/front-end/bloco_19/project-recipes-redux/src/App.js
+++ exercises/front-end/bloco_19/project-recipes-redux/src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Comidas from './components/Comidas';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route path="/comidas" component={ Comidas } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
