4deb1a344cd992e81a690bb839c9bba30a174d2a
Fernando Costa <fgosuendacosta@gmail.com>
2021-10-10 18:06:05 -0300

    Atualizando até requisito 21

--- exercises/back-end/bloco_19/dia_05/sd-012-mysql-all-for-one-master/desafio7.sql
+++ exercises/back-end/bloco_19/dia_05/sd-012-mysql-all-for-one-master/desafio7.sql
@@ -1,5 +1,5 @@
 --  7 - Mostre apenas os ids dos 5 últimos registros da tabela products (a ordernação deve ser baseada na coluna id).
 
 SELECT id FROM northwind.products
-LIMIT 5
-ORDER BY id DESC;
+ORDER BY id DESC
+LIMIT 5;
