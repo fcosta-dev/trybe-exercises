7691703cb3154b58d3ded89d5c06ddb6151e2194
Fernando Costa <fgosuendacosta@gmail.com>
2021-10-11 15:45:08 -0300

    Iniciando os exercícios

---
+++ exercises/back-end/bloco_20/dia_01/manipulacao_strings.sql
@@ -0,0 +1,86 @@
+SELECT UCASE(title) FROM sakila.film LIMIT 10;
+SELECT LCASE(title) FROM sakila.film LIMIT 10;
+SELECT REPLACE(title, 'ACADEMY', 'FOO') FROM sakila.film WHERE film_id = 1;
+SELECT LEFT(title, 7) FROM sakila.film WHERE film_id = 1;
+SELECT RIGHT(title, 8) FROM sakila.film WHERE film_id = 1;
+SELECT CHAR_LENGTH(title) FROM sakila.film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5, 2) FROM sakila.film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5) FROM sakila.film WHERE film_id = 1;
+
+SELECT *, IF (rental_rate > 0.99, 'CARO', 'BARATO')
+from sakila.film;
+
+SELECT title, rental_rate,
+	CASE
+		WHEN rental_rate = 0.99 THEN 'BARATO'
+        WHEN rental_rate = 2.99 THEN 'MEDIO'
+        WHEN rental_rate = 4.99 THEN 'CARO'
+	ELSE rental_rate = 'NÃO CLASSIFICADO'
+END AS valor
+FROM sakila.film
+LIMIT 10;
+
+SELECT CURRENT_DATE(); -- YYYY-MM-DD
+SELECT NOW(); -- YYYY-MM-DD HH:MM:SS
+
+
+-- 30, ou seja, a primeira data é 30 dias depois da segunda
+SELECT DATEDIFF('2020-01-31', '2020-01-01');
+
+-- -30, ou seja, a primeira data é 30 dias antes da segunda
+SELECT DATEDIFF('2020-01-01', '2020-01-31');
+
+-- -01:00:00, ou seja, há 1 hora de diferença entre os horários
+SELECT TIMEDIFF('08:30:10', '09:30:10');
+
+-- -239:00:00, ou seja, há uma diferença de 239 horas entre as datas
+SELECT TIMEDIFF('2021-08-11 08:30:10', '2021-08-01 09:30:10');
+
+
+SELECT YEAR(CURRENT_DATE()); -- retorna o ano atual
+SELECT HOUR(NOW()); -- retorna a hora atual
+
+
+-- GoupBy e Having
+SELECT first_name FROM sakila.actor
+GROUP BY first_name;
+
+-- Média de duração de filmes agrupados por classificação indicativa
+SELECT rating, AVG(length)
+FROM sakila.film
+GROUP BY rating;
+
+-- Valor mínimo de substituição dos filmes agrupados por classificação indicativa
+SELECT rating, MIN(replacement_cost)
+FROM sakila.film
+GROUP BY rating;
+
+-- Valor máximo de substituição dos filmes agrupados por classificação indicativa
+SELECT rating, MAX(replacement_cost)
+FROM sakila.film
+GROUP BY rating;
+
+-- Custo total de substituição de filmes agrupados por classificação indicativa
+SELECT rating, SUM(replacement_cost)
+FROM sakila.film
+GROUP by rating;
+
+SELECT first_name, COUNT(*)
+FROM sakila.actor
+GROUP BY first_name
+HAVING COUNT(*) > 2;
+
+-- Ou, melhor ainda, usando o AS para dar nomes às colunas de agregação,
+-- melhorando a leitura do resultado
+SELECT first_name, COUNT(*) AS nomes_cadastrados
+FROM sakila.actor
+GROUP BY first_name
+HAVING nomes_cadastrados > 2;
+
+-- Observação: o alias não funciona com strings para o HAVING,
+-- então use o underline ("_") para separar palavras
+-- Ou seja, o exemplo abaixo não vai funcionar
+SELECT first_name, COUNT(*) AS 'nomes cadastrados'
+FROM sakila.actor
+GROUP BY first_name
+HAVING 'nomes cadastrados' > 2;
