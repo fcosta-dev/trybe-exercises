a52aeb28580d4a728ed29a90389400be229a64e4
Fernando Costa <fgosuendacosta@gmail.com>
2021-10-18 08:06:00 -0300

    Atualizando sql

---
+++ exercises/back-end/bloco_20/dia_03/sql beemovies.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS BeeMovies;
+
+USE BeeMovies;
+
+CREATE TABLE movies(
+    movie_id INT PRIMARY KEY auto_increment,
+    ticket_price DECIMAL(12, 2) NOT NULL DEFAULT 0,
+    ticket_price_estimation VARCHAR(15),
+    release_year YEAR
+) engine = InnoDB;
+
+CREATE TABLE movies_logs(
+    log_id INT AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT NOT NULL,
+    executed_action VARCHAR(15) NOT NULL,
+    log_date DATE NOT NULL
+) engine = InnoDB;
