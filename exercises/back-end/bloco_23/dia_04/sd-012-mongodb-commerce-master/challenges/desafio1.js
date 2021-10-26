80f696e2c99481a85750aded89154be7d4ed119f
Fernando Costa <fgosuendacosta@gmail.com>
2021-10-26 14:16:30 -0300

    Iniciando projeto

---
+++ exercises/back-end/bloco_23/dia_04/sd-012-mongodb-commerce-master/challenges/desafio1.js
@@ -0,0 +1,6 @@
+// 1. Crie uma query que adicione o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
+
+
+
+
+// 2. Crie uma query que retorne o nome e criadoPor de todos os produtos.
