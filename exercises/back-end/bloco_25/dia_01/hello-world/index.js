a26cb05f222b3850965ab13a15301120d8acc8ac
Fernando Costa <fgosuendacosta@gmail.com>
2021-10-28 10:47:47 -0300

    Finalizando exercicios do conteudo

---
+++ exercises/back-end/bloco_25/dia_01/hello-world/index.js
@@ -0,0 +1,9 @@
+const readline = require('readline-sync')
+
+// const readline = require('readline-sync');
+
+const name = readline.question('Qual seu nome? ');
+const age = readline.questionInt('Qual sua idade? ');
+
+// console.log('Hello, world!');
+console.log(`Hello, ${name}! You are ${age} years old!`);
