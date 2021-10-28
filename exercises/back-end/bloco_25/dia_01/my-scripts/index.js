ab0c2abfcc7a212e495ec3a091a769aa008d25c3
Fernando Costa <fgosuendacosta@gmail.com>
2021-10-28 18:44:20 -0300

    Exercicio 8

---
+++ exercises/back-end/bloco_25/dia_01/my-scripts/index.js
@@ -0,0 +1,23 @@
+const readline = require('readline-sync');
+
+const scripts = [
+  { name: 'Calcular IMC', arquivo: './imc.js' },
+  { name: 'Calcular velocidade média', arquivo: './velocidade.js' },
+  { name: 'Jogo de adivinhação', arquivo: './sorteio.js' },
+];
+
+// Percorre os scrips para criar uma lista numerada para acessa-los
+let mensagem = scripts
+  .map((script, index) => `${index + 1} - ${script.name}`);
+
+// Adicionando uma linha a mais
+mensagem.unshift('Escolha um número para executar o script correspondente');
+
+mensagem = mensagem.join('\n');
+
+const scriptNumber = readline.questionInt(mensagem) - 1;
+
+const script = scripts[scriptNumber];
+
+if (!script) return console.log('Número inválido. Saindo');
+require(script.arquivo);
