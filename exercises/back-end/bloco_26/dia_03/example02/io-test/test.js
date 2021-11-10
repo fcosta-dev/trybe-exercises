e4602aadd7b658122fa211b98ecad613fc169839
Fernando Costa <fgosuendacosta@gmail.com>
2021-11-10 18:17:51 -0300

    Mocha

---
+++ exercises/back-end/bloco_26/dia_03/example02/io-test/test.js
@@ -0,0 +1,21 @@
+describe('leArquivo', () => {
+  describe('Quando o arquivo existe', () => {
+    describe('a resposta', () => {
+      it('é uma string', () => {
+        //
+      });
+
+      it('é igual ao conteúdo do arquivo', () => {
+        //
+      });
+    });
+  });
+
+  describe('Quando o arquivo não existe', () => {
+    describe('a resposta', () => {
+      it('é igual a "null"', () => {
+        //
+      });
+    });
+  });
+});
