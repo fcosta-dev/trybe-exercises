7a41228ac7091932931eeaa91df1b7b566c2dfca
Fernando Costa <fgosuendacosta@gmail.com>
2021-11-10 18:23:52 -0300

    Implementação do arquivo

--- exercises/back-end/bloco_26/dia_03/example02/io-test/leArquivo.js
+++ exercises/back-end/bloco_26/dia_03/example02/io-test/leArquivo.js
@@ -1,3 +1,13 @@
-module.exports = () => {
-  //
+const fs = require('fs');
+
+function leArquivo(nomeDoArquivo) {
+  try {
+    const conteudoDoArquivo = fs.readFileSync(nomeDoArquivo, 'utf8');
+
+    return conteudoDoArquivo;
+  } catch (err) {
+    return null;
+  }
 }
+
+module.exports = leArquivo;
