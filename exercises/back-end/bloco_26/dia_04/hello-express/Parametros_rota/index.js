cdf5148ce46efda1f5997814c0a3feeb4851d285
Fernando Costa <fgosuendacosta@gmail.com>
2021-11-19 10:08:27 -0300

    Subindo atualizaçoes

--- exercises/back-end/bloco_26/dia_04/hello-express/Parametros_rota/index.js
+++ exercises/back-end/bloco_26/dia_04/hello-express/Parametros_rota/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
+app.use(bodyParser.json())
 
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
@@ -34,6 +37,45 @@ app.get('/recipes/:id', function (req, res) {
 
   // Ou, retorna informações do recupe
   res.status(200).json(recipe);
+});
+
+// ******************************
+// Recebendo dados no body da requisição.
+// ******************************
+// Agora vamos implementar nossa rota que vai receber dados no body da requisição.
+// Perceba, que repetimos a rota /recipes , só que agora usando o método .post .
+app.post('/recipes', function (req, res) {
+  // desestruturamos os atributos id , price e preco do objeto req.body
+  const { id, name, price } = req.body;
+  // usamos esses valores para inserir um novo objeto dentro da array receitas
+  recipes.push({ id, name, price});
+  // retornamos uma resposta com o status 201, que serve para sinalizar que ocorreu uma operação de persistência de uma informação e um json com o atributo message
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
+// Diferente do que fizemos para fazer uma requisição do tipo GET , dessa vez passamos um segundo parâmetro que é um objeto formado pelos atributos method , headers , body .
+// * method: Método HTTP utilizado, como vimos no primeiro bloco temos 4 que são mais utilizados (GET, POST, PUT e DELETE).
+// * headers: Define algumas informações sobre a requisição como o atributo Accept que diz qual o tipo de dado esperado como resposta dessa requisição e o Content-Type que sinaliza que no corpo da requisição está sendo enviado um JSON.
+// * body: É o corpo da requisição. Como no HTTP só é possível trafegar texto, é necessário transformar o objeto JavaScript que quermos enviar para uma string JSON. Por isso que do lado do nosso back-end precisamos utilizar o bodyParser para transformar as informações que foram trafegadas como string JSON, de volta para um objeto JavaScript.
+fetch('http://localhost:3001/recipes/', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    id: 4,
+    name: 'Macarrão com Frango',
+    price: 30
+  })
+});
+
+// Precisamos ter uma rota que recebe um token para ser validado, a regra da validação é checar se o token possui 16 caracteres.
+app.get('/validateToken', function (req, res) {
+  const token = req.headers.authorization;
+  if (token.length !== 16) return res.status(401).json({message: 'Invalid Token!'})
+
+  res.status(200).json({message: 'Valid Token!'})
 });
 
 app.listen(3001, () => {
