7a6ed7e2d7226959dbf0ad7a02a7147df02fd987
Fernando Costa <fgosuendacosta@gmail.com>
2021-11-25 09:06:36 -0300

    Iniciando novo dia

---
+++ exercises/back-end/bloco_26/dia_04/exercises/bonus/Exercicio01/authMiddleware.js
@@ -0,0 +1,9 @@
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || authorization.length !== 16) {
+    return res.status(401).json({ message: 'Token inválido!' });
+  }
+
+  return next();
+}
