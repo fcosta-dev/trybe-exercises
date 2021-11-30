36ada0778c46d6e9fb73c896187d81a6e1f6ff43
Fernando Costa <fgosuendacosta@gmail.com>
2021-11-30 17:13:47 -0300

    Conteudos de router

---
+++ exercises/back-end/bloco_26/dia_05/conteudos/router/index.js
@@ -0,0 +1,22 @@
+/* index.js */
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Esta rota não passa pelo middleware de autenticação!
+app.get('/open', function (req, res) {
+  res.send('open!')
+});
+
+const recipesRouter = require('./recipesRouter');
+
+/* Todas as rotas com /recipes/<alguma-coisa> entram aqui e vão para o roteador. */
+app.use('/recipes', recipesRouter);
+
+// app.all('*', function (req, res) {
+//  return res.status(404).json({ message: `Rota '${req.path}' não existe!`});
+// });
+
+app.listen(3001, () => { console.log('Ouvindo na porta 3001'); });
