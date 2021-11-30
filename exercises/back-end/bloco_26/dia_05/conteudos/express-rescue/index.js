c345b2fbded8db416141d4352e063d2ff747a96b
Fernando Costa <fgosuendacosta@gmail.com>
2021-11-30 17:36:01 -0300

    Pacote express rescue

---
+++ exercises/back-end/bloco_26/dia_05/conteudos/express-rescue/index.js
@@ -0,0 +1,25 @@
+/* index.js */
+const express = require('express');
+const rescue = require('express-rescue');
+const errorMiddleware = require('./errorMiddleware');
+
+const app = express();
+
+app.get('/:fileName', [
+  rescue(async (req, res) => {
+    const file = await fs.readFile(`./${req.params.fileName}`);
+    res.send(file.toString('utf-8'));
+  }),
+  (err, req, res, next) => {
+    if (err.code === 'ENOENT') {
+      const newError = new Error(err.message);
+      newError.code = 'file_not_found';
+      newError.status = 404;
+      return next(newError);
+    }
+
+    return next(err);
+  },
+]);
+
+app.use(errorMiddleware);
