8d8ee5cc659ebc4e71c51ecfe6638973b49916d2
Fernando Costa <fgosuendacosta@gmail.com>
2021-12-01 16:02:14 -0300

    Primeiro requisito

--- exercises/back-end/bloco_26/dia_06/sd-011-project-talker-manager-master/routes/talkerRoutes.js
+++ exercises/back-end/bloco_26/dia_06/sd-011-project-talker-manager-master/routes/talkerRoutes.js
@@ -5,21 +5,11 @@ const router = express.Router();
 // Importa o fs para trabalhar a leitura de arquivos
 const fs = require('fs').promises;
 
-// Importa o Middleware de validação de conteudo do arquivo a ser lido, no caso o talker.json
-const validateFileContent = require('../middlewares/validateFileContent');
+router.get('/', async (_req, res) => {
+  const fileContent = await fs.readFile('./talker.json');
+  const people = JSON.parse(fileContent);
 
-router.get(
-  '/', // Rota raíz
-  validateFileContent, // Aciona o primeiro middleware
-  async (_req, res, _next) => { // Aciona o segundo middleware
-    try {
-      const fileContent = await fs.readFile('./talker.json'); // Faz a leitura no arquivo JSON com o fs
-      const people = JSON.parse(fileContent);
-      return res.status(200).json(people); // Recebe o status 200 e monta lista das pessoas
-    } catch (error) {
-      console.log(error); // Joga no console o erro encontrado, caso aconteça
-    }
-  },
-);
+  return res.status(200).json(people);
+});
 
 module.exports = router; 
