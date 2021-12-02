4547ee08c43fba1b600dc4ae1b0f54a0d11d0de5
Fernando Costa <fgosuendacosta@gmail.com>
2021-12-02 13:36:54 -0300

    Aprendendo sobre o conteudo Model

---
+++ exercises/back-end-14/bloco_23/dia_01/model-example/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const Author = require('./models/Author');
+
+const app = express();
+
+app.get('/authors', async (_req, res) => {
+  const authors = await Author.getAll();
+  res.status(200).json(authors);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Ouvindo a porta ${PORT}`);
+});
