062e7a41c4df3933efaeaec960bb27652fa32861
Fernando Costa <fgosuendacosta@gmail.com>
2021-12-09 18:59:51 -0300

    Finalizando bonus

--- exercises/back-end-14/bloco_23/dia_02/cep-lookup/services/Cep.js
+++ exercises/back-end-14/bloco_23/dia_02/cep-lookup/services/Cep.js
@@ -3,32 +3,31 @@ const Cep = require('../models/Cep');
 const CEP_REGEX = /\d{5}-?\d{3}/;
 
 const findAddressByCep = async (searchedCep) => {
-  // Valida o CEP e em caso dele ser falso, retorna um erro
-  if (!CEP_REGEX.test(cep)) {
-    return {
-      erro: {
-        code: 'invalidData',
-        message: 'CEP iválido',
-      }
-    }
-  }
-
-  // Buscamos o CEP através do Model
+  // Começamos buscando o CEP no banco de dados
   const cep = await Cep.findAddressByCep(searchedCep);
-
-  // Caso não encontre nenhum CEP, o service retorna um objeto de erro.
-  if (!cep) {
+  // Caso encontremos, retornamos sem consultar a API
+  if (cep) {
+    return cep;
+  }
+  // Caso o CEP não exista no banco de dados, buscamos na API
+  const cepFromApi = await ViaCep.lookupCep(searchedCep);
+  // Caso o CEP não exista na API,
+  // retornamos um erro dizendo que
+  // o CEP não foi encontrado
+  if (!cepFromApi) {
     return {
       error: {
         code: 'notFound',
-        message: 'CEP não encontrado'
+        message: 'CEP não encontrado',
       },
     };
   }
 
-  // Por fim, retornamos o CEP correto
-  return cep;
-}
+  // Caso o CEP exista na API, pedimos ao model
+  // que armazene-o no banco e retornamos
+  // o resultado
+  return Cep.create(cepFromApi);
+};
 
 const create = async ({ cep, logradouro, bairro, localidade, uf }) => {
   // Começamos buscando o cep que estamos tentando cadastrar
